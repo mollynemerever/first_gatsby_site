b7257a25d10b91a44be817054308a7516b18e27e
github username <github email address>
2019-09-22 15:11:04 -0700

    preliminary contact form styling complete

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -8,35 +8,42 @@ const ThirdPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact Me</h1>
-    <p>Welcome to page 3</p>
-    <form action="https://formspree.io/davidlee327@gmail.com" method="POST">
-      <label>
-        <input placeholder="Name" type="text" required="required" name="name" />
-      </label>{" "}
-      <br />
-      <label>
-        <input
-          placeholder="Email"
-          type="email"
-          name="_replyto"
-          required="required"
-        />
-      </label>
-      <br />
-      <label>
-        <input
-          placeholder="Message"
-          type="text"
-          required="required"
-          name="message"
-        />
-      </label>
-      <br />
-      <button type="submit" value="Send">
-        {" "}
-        Send{" "}
-      </button>
-    </form>
+    <p>Feel free to email me, I'd love to meet you!</p>
+    <div id="contact-form">
+      <form action="https://formspree.io/davidlee327@gmail.com" method="POST">
+        <label>
+          <input
+            placeholder="Name"
+            type="text"
+            required="required"
+            name="name"
+          />
+        </label>{" "}
+        <br />
+        <label>
+          <input
+            placeholder="Email"
+            type="email"
+            name="_replyto"
+            required="required"
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            placeholder="Message"
+            type="text"
+            required="required"
+            name="message"
+          />
+        </label>
+        <br />
+        <button type="submit" value="Send">
+          {" "}
+          Send{" "}
+        </button>
+      </form>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
